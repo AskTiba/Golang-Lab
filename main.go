caa0167e9235fdf9514316ff4ce9a1a10faf9ad4
Tibamwenda Anthony <anthonyngisiro@gmail.com>
2025-01-08 14:25:07 +0300

    Numbers and maths functions

--- main.go
+++ main.go
@@ -2,43 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math"
 )
 
 func main() {
-    house := "Stark"
-    name := "Red"
-	words := "Winter is coming!"
-    fmt.Println("Hello",name, house, ".Tell them that", words, "!")
+	phi := math.Phi
+	fmt.Println("The value of phi is:",phi)
+	fmt.Println("phi (Round):", math.Round(phi))
+	fmt.Println("phi (Ceil):", math.Ceil(phi))
+	fmt.Println("phi (Floor):", math.Floor(phi))
 
-	IsPresent := true
-	fmt.Println("It is", !IsPresent, "that United lost today.")
+	besty := math.Abs(-13) 
+	fmt.Println("The absolute value of -13 is:", besty)
 
-	str1 := "Welcome to Jamaica"
-	str2 := "welcome to jamaica"
-	fmt.Println(strings.EqualFold(str1,str2))
+	exponent := math.Pow(2,16)
+	fmt.Println("2 exponent 16 is:", exponent)
 
-	fmt.Println(strings.Index(name,`d`))
-	fmt.Println(len(house))
-
-	str3 := "Dorothy"
-	isIndex := strings.Index(words,`is`)
-	fmt.Println(words[isIndex+3:16])
-	// fmt.Println(strings.Index(str3,`y`))
-	fmt.Println(str3[:4])
-	
-	// Numbers, F = 9C/5 + 32
-	// F = (K-273.15)*1.8+32, where K = 288.15
-	temperatureK := 288.15
-	temperatureF := 1.8*(temperatureK-273.15) + 32
-	fmt.Println("Temperature in Kelvin:", temperatureK)
-	fmt.Println("Temperature in Fahreheit:", temperatureF)
-
-	// when F = 70, K = ?
-	temperatureFah := 70.0
-	temperatureKel := ((temperatureFah-32)/(1.8))+273.15
-	fmt.Println("Temperature in Fahreheit:", temperatureFah)
-	fmt.Println("Temperature in Kelvin:", temperatureKel)
-
-
+	squareRoot := math.Sqrt(144)
+	fmt.Println("The square root of 144 is:",squareRoot)
 }
