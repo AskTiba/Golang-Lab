a47bb1b70b4911216c676b1bad9c7f0574f02ab5
Tibamwenda Anthony <anthonyngisiro@gmail.com>
2025-01-10 18:22:33 +0300

    Slices

--- main.go
+++ main.go
@@ -4,28 +4,21 @@ import "fmt"
 
 // Main function to run the game.
 func main() {
-	// var bodyType [4]string
-	// bodyType[0] = "Slim"
-	// bodyType[1] = "Slender"
-	// bodyType[2] = "Curvy"
+	fuelTypes := []string{"Gas","Electric","Hybrid","Others"}
+	fuelTypes = append(fuelTypes, "Diesel")
+	fmt.Println(len(fuelTypes))
+	fmt.Println(fuelTypes)
+	fmt.Println("The slice is nill:", fuelTypes == nil)
+	fmt.Println()
 
-		// var bodyType [4]string
+	iceCreamFlavour := make([]string,2)
+	fmt.Println(len(iceCreamFlavour))
+	fmt.Println("The slice is nill:", iceCreamFlavour == nil)
+	fmt.Println(iceCreamFlavour)
+	iceCreamFlavour[0] = "Strawberry"
+	iceCreamFlavour[1] = "Pistachio"
+	iceCreamFlavour = append(iceCreamFlavour,"Vanilla","Chocholate","Hazelnut")
+	fmt.Println(len(iceCreamFlavour))
+	fmt.Println(iceCreamFlavour)
 
-	// bodyType := [4]string{"Slender","Curvy","Petite"}
-	// fmt.Println(bodyType)
-
-	var bodyTypes [3][2]string
-	bodyTypes[0] = [2]string{"4 petites available", "3 petites booked"}
-	bodyTypes[1] = [2]string{"6 slenders available", "1 slender booked"}
-	bodyTypes[2] = [2]string{"2 curvys available", "3 curvys booked"}
-	fmt.Println("Body type status")
-
-	for i := 0; i < len(bodyTypes); i++ {
-		row := bodyTypes[i]
-		for j := 0; j < len(row); j++ {
-			fmt.Printf("%v ",row[j])
-		}
-		fmt.Println()
-	}
-	
 }
